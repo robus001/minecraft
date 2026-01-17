4ea10da7854b87ac4ef6cf0109d922fc07065d08
robus001 <138107047+robus001@users.noreply.github.com>
2026-01-17 15:02:01 +0200

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,1 +1,15 @@
+function addLog(action) {
+  const user = JSON.parse(localStorage.getItem("session"));
+  if (!user) return;
+
+  const logs = JSON.parse(localStorage.getItem("logs") || "[]");
+
+  logs.push({
+    admin: user.name,
+    action: action,
+    date: new Date().toLocaleString()
+  });
+
+  localStorage.setItem("logs", JSON.stringify(logs));
+}
 
