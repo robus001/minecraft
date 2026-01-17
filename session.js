51009b0a13213fc2823a3d4d7c43ff1367c6ae76
robus001 <138107047+robus001@users.noreply.github.com>
2026-01-17 15:07:54 +0200

    Update session.js

--- session.js
+++ session.js
@@ -1,1 +1,22 @@
+function login(email, password) {
+  const user = USERS.find(u => u.email === email && u.password === password);
+  if (!user) return alert("Date greșite");
+
+  localStorage.setItem("session", JSON.stringify(user));
+
+  if (user.role === "owner") {
+    location.href = "../owner/";
+  } else {
+    location.href = "../admin/";
+  }
+}
+
+function getUser() {
+  return JSON.parse(localStorage.getItem("session"));
+}
+
+function logout() {
+  localStorage.removeItem("session");
+  location.href = "../";
+}
 
