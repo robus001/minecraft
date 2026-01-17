8763c1a7f8b86e1f52b1a1296824dfa98be1c80b
robus001 <138107047+robus001@users.noreply.github.com>
2026-01-17 15:01:53 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,28 @@
+function login(email, password) {
+  const user = USERS.find(
+    u => u.email === email && u.password === password
+  );
+
+  if (!user) {
+    alert("Email sau parolă greșită");
+    return;
+  }
+
+  localStorage.setItem("session", JSON.stringify(user));
+
+  if (user.role === "owner") {
+    window.location.href = "../owner/";
+  } else {
+    window.location.href = "../admin/";
+  }
+}
+
+function getUser() {
+  return JSON.parse(localStorage.getItem("session"));
+}
+
+function logout() {
+  localStorage.removeItem("session");
+  window.location.href = "../";
+}
 
