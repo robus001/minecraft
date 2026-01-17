011ca639d54ff5665e5605fb9b0647193aa395ea
robus001 <138107047+robus001@users.noreply.github.com>
2026-01-17 15:01:42 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,1 +1,21 @@
+const USERS = [
+  {
+    role: "owner",
+    name: "Robus",
+    email: "owner@server.ro",
+    password: "owner123"
+  },
+  {
+    role: "admin",
+    name: "Alex",
+    email: "alex@server.ro",
+    password: "admin123"
+  },
+  {
+    role: "admin",
+    name: "Mihai",
+    email: "mihai@server.ro",
+    password: "admin456"
+  }
+];
 
